38c90e661865ea7be7c4a7e0811cae9a3d8a8028
Denis_Kiprenko <deniskiprenko@yandex.ru>
2023-05-09 23:28:05 +1000

    Добавил Д.З.6

---
+++ H_W_6/H_W_6.1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+string EnterData(string text)
+{
+    Console.WriteLine(text);
+    return ((Console.ReadLine()));
+}
+
+int CountNumbers(string[] massive)
+{
+    int sum = 0;
+    for (int i = 0; i < massive.Length; i++)
+    {
+        int num = int.Parse(massive[i]);
+        if(num > 0)
+        {
+        sum++;
+        }
+    }
+    return sum;
+}
+
+string numbers = EnterData("Введите числа через  запятую : ");
+string[] massive = numbers.Split(",");
+int count = CountNumbers(massive);
+Console.WriteLine($"{String.Join(", ",massive)} -> {count}");
---
+++ H_W_6/H_W_6.2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+
+double EnterData(string text)
+{
+    Console.WriteLine(text);
+    return (double.Parse(Console.ReadLine()));
+}
+
+double b1 = EnterData("Введите число b1: ");
+double k1 = EnterData("Введите число k1: ");
+double b2 = EnterData("Введите число b2: ");
+double k2 = EnterData("Введите число k2: ");
+double x = (b2-b1)/(k1-k2);
+double y = (k1*x+b1);
+Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x}; {y})");
